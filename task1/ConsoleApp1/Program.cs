27ed0e29530f9370b16bedc2521377b034f10e0c
Argen <argenski@hotmail.com>
2023-03-30 22:06:38 +0600

    create project hw45

---
+++ task1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+
+            listener.Prefixes.Add("http://localhost:8888/hello_world/");
+            listener.Prefixes.Add("http://localhost:8888/main_page/");
+            listener.Start();
+
+            Console.WriteLine("Ждем подключений");
+
+            HttpListenerContext context = listener.GetContext();
+
+            HttpListenerResponse response = context.Response;
+            string fileName = context.Request.Url.AbsolutePath;
+
+            string str = "";
+
+
+
+            if (fileName == "/hello_world/")
+            {
+                str = "<html><head><meta charset='utf8'><style> body{ background-color:red; color:white;} </style></head><body> <h1>hello_world</h1></body></html>";
+            } 
+            else if (fileName == "/main_page/")
+            {
+                str = "<html><head><meta charset='utf8'><style> body{background-color:gray; color:red;}</style></head><body> <h1>main_page</h1></body></html>";
+            }
+                
+
+
+
+
+
+
+            byte[] buffer = Encoding.UTF8.GetBytes(str);
+
+            response.ContentLength64 = buffer.Length;
+
+            Stream output = response.OutputStream;
+
+            output.Write(buffer, 0, buffer.Length);
+
+            output.Close();
+
+            listener.Stop();
+
+            Console.WriteLine("Соединение закрыто");
+
+            Console.Read();
+        }
+    }
+}
---
+++ task2/Lesson45/Server.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson45
+{
+    internal class Server
+    {
+        private Thread _serverThread;
+        private string _siteDirectory;
+        private HttpListener _listener;
+        private int _port;
+        public Server(string path, int port)
+        {
+            Initialize(path, port);
+        }
+
+        private void Initialize(string path, int port)
+        {
+            _siteDirectory = path;
+            _port = port;
+            _serverThread = new Thread(Listen);
+            _serverThread.Start();
+            Console.WriteLine("Сервер запущен на порту " + _port);
+            Console.WriteLine("файлы лежат в папке " + _siteDirectory);
+        }
+
+        private void Listen(object? obj)
+        {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add($"http://localhost:{_port}/");
+            _listener.Start();
+            while (true)
+            {
+                try
+                {
+                    HttpListenerContext context = _listener.GetContext();
+                    Process(context);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        private void Process(HttpListenerContext context)
+        {
+            string fileName = context.Request.Url.AbsolutePath;
+            Console.WriteLine(fileName);
+            if(fileName == "/")
+            {
+                fileName = _siteDirectory + fileName+"index.html";
+                if (File.Exists(fileName))
+                {
+                    try
+                    {
+                        Stream fileStream = new FileStream(fileName, FileMode.Open);
+                        context.Response.ContentType = GetContentType(fileName);
+                        context.Response.ContentLength64 = fileStream.Length;
+                        context.Response.StatusCode = (int)HttpStatusCode.OK;
+                        byte[] buffer = new byte[16 * 1024];
+                        int dataLength;
+                        do
+                        {
+                            dataLength = fileStream.Read(buffer, 0, buffer.Length);
+                            context.Response.OutputStream.Write(buffer, 0, dataLength);
+                        }
+                        while (dataLength > 0);
+                        fileStream.Close();
+                        context.Response.OutputStream.Flush();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    }
+                }
+                else
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                }
+            }
+            else if(fileName == "/white_rabbit/"|| fileName == "/white_rabbit/styles.css")
+            {
+                if(fileName == "/white_rabbit/styles.css")
+                {
+                    fileName = fileName.Remove(0, 13);
+                    fileName = _siteDirectory + fileName;
+                }
+                else
+                {
+                    fileName = fileName.Remove(fileName.Length - 1);
+                    fileName = _siteDirectory + fileName + ".html";
+                }
+                
+                
+
+                if (File.Exists(fileName))
+                {
+                    try
+                    {
+                        Stream fileStream = new FileStream(fileName, FileMode.Open);
+                        context.Response.ContentType = GetContentType(fileName);
+                        context.Response.ContentLength64 = fileStream.Length;
+                        context.Response.StatusCode = (int)HttpStatusCode.OK;
+                        byte[] buffer = new byte[16 * 1024];
+                        int dataLength;
+                        do
+                        {
+                            dataLength = fileStream.Read(buffer, 0, buffer.Length);
+                            context.Response.OutputStream.Write(buffer, 0, dataLength);
+                        }
+                        while (dataLength > 0);
+                        fileStream.Close();
+                        context.Response.OutputStream.Flush();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    }
+                }
+            }
+            else
+            {
+                fileName = "";
+                fileName = _siteDirectory + "/501.html";
+                if (File.Exists(fileName))
+                {
+                    
+
+                    Stream fileStream = new FileStream(fileName, FileMode.Open);
+                    context.Response.ContentType = GetContentType(fileName);
+                    context.Response.ContentLength64 = fileStream.Length;
+                    context.Response.StatusCode = (int)HttpStatusCode.NotImplemented;
+                    byte[] buffer = new byte[16 * 1024];
+                    int dataLength;
+                    do
+                    {
+                        dataLength = fileStream.Read(buffer, 0, buffer.Length);
+                        context.Response.OutputStream.Write(buffer, 0, dataLength);
+                    }
+                    while (dataLength > 0);
+                    fileStream.Close();
+                    context.Response.OutputStream.Flush();
+                }
+            }
+            
+            context.Response.OutputStream.Close();
+        }
+
+        private string GetContentType(string fileName)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>
+            {
+                {".css", "text/css"},
+                {".html", "text/html"},
+                {".ico", "image/x-icon" },
+                {".js", "application/x-javascript" },
+                {".json", "application/json" },
+                {".png", "image/png" }
+            };
+            string contentType = "";
+            string fileExt = Path.GetExtension(fileName);
+            dictionary.TryGetValue(fileExt, out contentType);
+            return contentType;
+        }
+
+        public void Stop() {
+            _serverThread.Abort();
+            _listener.Stop();
+        }
+    }
+}
